536a4f7babfae7fc9b8551b09cf1f3aa5628c9c6
fregnani123 <fabianofregnani@hotmail.com>
2023-04-06 15:21:52 -0300

    atualizar

---
+++ args.js
@@ -0,0 +1,10 @@
+    module.exports = {
+
+        mult (x,y){
+            return x*y
+        },
+        
+       soma (x,y){
+            return x + y
+        }
+    }
--- treino.js
+++ treino.js
@@ -1,5 +1,5 @@
 let args = process.argv.slice(2)
-
+let somaMult = require('./args')
 
 
 let a = Number(args[1])
@@ -7,19 +7,13 @@ let b = Number(args[2])
 let c = "";
 
 if (args[0]==="s"){
-    c= soma(a,b)
+    c= somaMult.soma(a,b)
 }else if(args[0]==="m"){
-    c= mult(a,b)
+    c= somaMult.mult(a,b)
 }else{
     c = 'opcão invalida'
 }
 
-function mult (x,y){
-    return x*y
-}
 
-function soma (x,y){
-    return x + y
-}
 
 console.log (c)
