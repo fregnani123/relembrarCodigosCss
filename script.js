fd8e0a476bb2e32fe8753e72a9424c185db406ab
fregnani123 <fabianofregnani@hotmail.com>
2023-04-07 14:50:54 -0300

    atualizar

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 
 const btnMenu = document.querySelector('.btnMenu').addEventListener('click', toglleMenu)
 
-
 function toglleMenu(e){
     if(e.type === 'touchstart') e.preventDefault();
     const navBar = document.querySelector('.nav');
--- server.js
+++ server.js
@@ -1,13 +1,31 @@
-let http = require('http')
-const url = require('url')
+let http = require('http');
+let url = require('url');
+let path = require('path');
+let fs = require('fs');
 
-http.createServer((req,res)=> {
-    let path = url.parse(req.url);  
-  res.end("oii")
-}).listen(3000, (error) => {
-    if (error){
+
+http.createServer((req,res) => {
+path = url.parse(req.url).pathname;
+
+let fileName = "." + path;
+
+fs.readFile(fileName, (err, data) => {
+    if(err){
+        res.writeHead(404,{'Content-Type':'text/html'});
+        res.end('Página não encontrada');
+    }else{
+        res.writeHead(200,{'Content-Type': 'text/html'})
+        res.write(data);
+        res.end()
+    }
+})
+
+ res.end()
+
+}).listen(3000, (error) =>  {
+    if(error){
         console.log(error)
     }else{
-        console.log('servidor rodando na porta 3000')
+        console.log('Servidor rodando na porta 3000')
     }
-} )
+})
