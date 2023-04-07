51e3e7b0a313e3acf70e9b0d416a4ec4199525d4
fregnani123 <fabianofregnani@hotmail.com>
2023-04-07 15:41:11 -0300

    server

--- server.js
+++ server.js
@@ -1,31 +1,58 @@
-let http = require('http');
-let url = require('url');
-let path = require('path');
-let fs = require('fs');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const url = require('url');
 
+const server = http.createServer((req, res) => {
+  const { pathname } = url.parse(req.url);
 
-http.createServer((req,res) => {
-path = url.parse(req.url).pathname;
-
-let fileName = "." + path;
-
-fs.readFile(fileName, (err, data) => {
-    if(err){
-        res.writeHead(404,{'Content-Type':'text/html'});
-        res.end('Página não encontrada');
-    }else{
-        res.writeHead(200,{'Content-Type': 'text/html'})
+  if (pathname === '/') {
+    fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.write('Erro interno do servidor');
+        res.end();
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write(data);
-        res.end()
-    }
-})
-
- res.end()
-
-}).listen(3000, (error) =>  {
-    if(error){
-        console.log(error)
-    }else{
-        console.log('Servidor rodando na porta 3000')
-    }
-})
+        res.end();
+      }
+    });
+  } else if (pathname.endsWith('.css')) {
+    const cssPath = path.join(__dirname, pathname);
+    fs.readFile(cssPath, (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.write('Erro interno do servidor');
+        res.end();
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/css' });
+        res.write(data);
+        res.end();
+      }
+    });
+  } else if (pathname.endsWith('.js')) {
+    const jsPath = path.join(__dirname, pathname);
+    fs.readFile(jsPath, (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.write('Erro interno do servidor');
+        res.end();
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/javascript' });
+        res.write(data);
+        res.end();
+      }
+    });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.write('Página não encontrada');
+    res.end();
+  }
+}).listen(3000, 'localhost', (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Servidor rodando na posta 3000');
+  }
+});
