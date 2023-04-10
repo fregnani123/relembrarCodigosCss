e066221e4e8fd769f91f8e978ccf86c602cfd33e
fregnani123 <fabianofregnani@hotmail.com>
2023-04-10 15:50:39 -0300

    estudar

--- server2.js
+++ server2.js
@@ -4,12 +4,16 @@ const path = require('path');
 const fs = require('fs');
 
 
-const server2 = http.createServer((req,res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    res.end('Hello World')
+http.createServer((req,res)=>{
+  
+    const{ pathname } = url.parse(req.url)
 
-}).listen(3000, (err)=> {
-   if(err){console.log(err)}else{
-    console.log('Servidor2 rodando na porta 3000')
-   }
-})
+    res.end()
+
+}).listen(3000,(err)=> {
+    if(err){
+        console.log(err)}else{
+            console.log('Servidor rodando na porta 3000')
+        }
+    }
+)
