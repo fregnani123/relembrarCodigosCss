d92332e7a46de1416b41976264c16c5df22c3feb
fregnani123 <fabianofregnani@hotmail.com>
2023-04-10 16:32:21 -0300

    atualizar

--- server2.js
+++ server2.js
@@ -1,19 +1,57 @@
 const http = require('http');
-const url = require('url');
-const path = require('path');
 const fs = require('fs');
+const path = require('path');
+const url = require('url');
 
-
-http.createServer((req,res)=>{
-  
-    const{ pathname } = url.parse(req.url)
-
-    res.end()
-
-}).listen(3000,(err)=> {
-    if(err){
-        console.log(err)}else{
-            console.log('Servidor rodando na porta 3000')
-        }
-    }
-)
+const server = http.createServer((req, res) => {
+  const { pathname } = url.parse(req.url);
+  if (pathname === '/') {
+    fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.write('Erro interno do servidor');
+        res.end();
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        res.end();
+      }
+    });
+  } else if (pathname.endsWith('.css')) {
+    const cssPath = path.join(__dirname, pathname);
+    fs.readFile(cssPath, (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.write('Erro interno do servidor');
+        res.end();
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/css' });
+        res.write(data);
+        res.end();
+      }
+    });
+  } else if (pathname.endsWith('.js')) {
+    const jsPath = path.join(__dirname, pathname);
+    fs.readFile(jsPath, (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.write('Erro interno do servidor');
+        res.end();
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/javascript' });
+        res.write(data);
+        res.end();
+      }
+    });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.write('Página não encontrada');
+    res.end();
+  }
+}).listen(3000, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Servidor rodando na posta 3000');
+  }
+});
