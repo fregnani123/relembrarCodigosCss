2c3ea165f4fd1e55b09a06dae72791da1c817225
fregnani123 <fabianofregnani@hotmail.com>
2023-04-14 17:40:51 -0300

    atualizar estudos

---
+++ shuflleCards.js
@@ -0,0 +1,18 @@
+
+
+let cards = ['A','B','C','D']
+
+let newCards = shuffleCards(cards)
+
+function shuffleCards(cards){
+  let currentIndex = cards.lengh;
+  let randomIndex = 0;
+
+  while (currentIndex !== 0){
+    randomIndex = Math.floor(Math.random = currentIndex);
+    currentIndex--;
+
+    
+  }
+
+}
